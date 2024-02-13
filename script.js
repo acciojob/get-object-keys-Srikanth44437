944b65333ab61a245e7d6497fe6319a801e3f653
Srikanth44437 <113106500+Srikanth44437@users.noreply.github.com>
2024-02-13 12:36:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const student = {
+	name:"Aravind"
+};
+Object.prototype.getKeys = function(){
+	return Object.keys(this);
+};
+const key = student.getKeys();
+console.log(key);
